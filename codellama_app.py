4117e9b858921e199e53920a679991d927a14813
mukundmurali-mm <44229735+mukundmurali-mm@users.noreply.github.com>
2024-02-06 07:51:25 +0530

    Create codellama_app.py

---
+++ codellama_app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import replicate
+import os
+
+# App title
+st.set_page_config(page_title="Codellama Chatbot")
+
+# Replicate Credentials
+with st.sidebar:
+	st.title('🦙💬 Codellama Chatbot')
+	if 'REPLICATE_API_TOKEN' in st.secrets:
+		st.success('API key already provided!', icon='✅')
+		replicate_api = st.secrets['REPLICATE_API_TOKEN']
+	else:
+		replicate_api = st.text_input('Enter Replicate API token:', type='password')
+		if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
+			st.warning('Please enter your credentials!', icon='⚠️')
+		else:
+			st.success('Proceed to entering your prompt message!', icon='👉')
+
+# Selecting the model
+	st.subheader('Models and parameters')
+	selected_model = 'codellama-70b-instruct'
+
+	llm = 'meta/codellama-70b-instruct:a279116fe47a0f65701a8817188601e2fe8f4b9e04a518789655ea7b995851bf'
+
+
+	temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+	top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+	max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
+
+#st.markdown('📖 Learn how to build this app in this [blog](https://medium.com/@mukundmurali/build-streamlit-application-with-codellama-llm-b0550afe703c)!')
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+	st.session_state.messages = [{"role": "assistant", "content": "How may I assist you in coding today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+	with st.chat_message(message["role"]):
+		st.write(message["content"])
+
+
+def clear_chat_history():
+	st.session_state.messages = [{"role": "assistant", "content": "How may I assist you in coding today?"}]
+
+
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+
+# Function for generating Code llama response
+def generate_llama2_response(prompt_input):
+	string_dialogue = "You are a helpful programming assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+	for dict_message in st.session_state.messages:
+		if dict_message["role"] == "user":
+			string_dialogue += "User: " + dict_message["content"] + "\n\n"
+		else:
+			string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+	output = replicate.run(llm,
+	                       input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+	                              "temperature": temperature, "top_p": top_p, "max_length": max_length,
+	                              "repetition_penalty": 1})
+	return output
+
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+	st.session_state.messages.append({"role": "user", "content": prompt})
+	with st.chat_message("user"):
+		st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+	with st.chat_message("assistant"):
+		with st.spinner("Thinking..."):
+			response = generate_llama2_response(prompt)
+			placeholder = st.empty()
+			full_response = ''
+			for item in response:
+				full_response += item
+				placeholder.markdown(full_response)
+			placeholder.markdown(full_response)
+	message = {"role": "assistant", "content": full_response}
+	st.session_state.messages.append(message)
